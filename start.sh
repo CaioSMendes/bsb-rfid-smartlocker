e6d3cfe34371088363ef25be9a5ea5bcd59582ce
CaioSMendes <caioitallo.souzamendes@gmail.com>
2025-01-08 08:22:43 -0300

    add new command bash start

--- start.sh
+++ start.sh
@@ -4,5 +4,14 @@ if [ -f tmp/pids/server.pid ]; then
 fi
 # Instala as Gems
 bundle check || bundle install
+
+# Check if the database needs to be set up
+if [ "$FIRST_TIME_SETUP" = "true" ]; then
+  echo "Running first time setup: db:create, db:migrate, db:seed"
+  bundle exec rails db:create db:migrate db:seed
+else
+  echo "Skipping first time setup."
+fi
+
 # Roda nosso servidor
 bundle exec puma -C config/puma.rb
