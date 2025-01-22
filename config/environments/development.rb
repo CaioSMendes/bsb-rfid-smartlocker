9fd7880bf90e1de3ed726702900980883ce02895
Caio Mendes <38328503+CaioSMendes@users.noreply.github.com>
2025-01-22 10:40:03 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   #config.hosts << "yellow-eggs-lie.loca.lt"
   #config.hosts << /[a-f0-9]+\.serveo\.net/ #ISSO FUNCIONA
   config.hosts << "smartlocker.brasiliarfid.com.br"
+  Rails.application.config.session_store :cookie_store, key: 'smartlocker', domain: :all, tld_length: 2
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
