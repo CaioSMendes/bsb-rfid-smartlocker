86318b8d3245a9379f61d41584c1c4c5f34a2b91
Caio Mendes <38328503+CaioSMendes@users.noreply.github.com>
2025-01-28 22:40:29 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,8 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  config.hosts << "smartlockerbrasiliarfid.com.br"  
   #config.hosts << "smartlocker.brasiliarfid.com.br"
-  config.hosts.clear
   #config.hosts << "srv708192.hstgr.cloud"
   #config.force_ssl = true
   #Rails.application.routes.default_url_options = { protocol: 'https' }
