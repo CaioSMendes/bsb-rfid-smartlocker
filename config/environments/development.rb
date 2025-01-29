02fde5efc16ca5070a9a861605ab1d2ba2560a09
Caio Mendes <38328503+CaioSMendes@users.noreply.github.com>
2025-01-28 22:30:46 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.server_timing = true
   #config.hosts << "smartlocker.brasiliarfid.com.br"
   config.hosts << "srv708192.hstgr.cloud"
-  #config.force_ssl = false
+  config.force_ssl = true
   #Rails.application.routes.default_url_options = { protocol: 'https' }
   #Rails.application.config.session_store :cookie_store, key: 'smartlocker', domain: :all, tld_length: 2
   config.log_level = :debug
