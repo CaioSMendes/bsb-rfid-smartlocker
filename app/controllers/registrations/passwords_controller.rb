065f99b37e7d89b9ea4bb5045890f4432dfe391c
CaioSMendes <caioitallo.souzamendes@gmail.com>
2025-07-11 10:18:09 -0300

    msg de alerta devise

--- app/controllers/registrations/passwords_controller.rb
+++ app/controllers/registrations/passwords_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Registrations::PasswordsController < Devise::PasswordsController
+   def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      # ⚡️ Adiciona mensagem no errors:
+      resource.errors.add(:email, "do usuário ou colaborador não cadastrado")
+      respond_with_navigational(resource) { render :new }
+    end
+  end
   # GET /resource/password/new
   # def new
   #   super
